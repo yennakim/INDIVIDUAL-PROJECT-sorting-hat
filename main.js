8cee10e45ed2fa5df419d35b8dd8d39baa49aeb1
Daun Kim <yennakim11@gmail.com>
2023-10-29 14:09:13 -0500

    created render to DOM function and rendered form

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
 const sortBtn = document.querySelector("#sortBtn");
+const form = document.querySelector("#studentForm");
+
+
+const renderToDom = (divId, htmlToRender) => {
+  const selectedDiv = document.querySelector(divId);
+  selectedDiv.innerHTML = htmlToRender;
+}
+
+const formOnDom = () => {
+  let domString = "";
+domString += `Student Name: <div class="form-floating mb-3">
+<input type="text" class="form-control" id="studentName" placeholder="Harry Potter">
+<label for="studentName">Name</label>
+</div>`;
+renderToDom("#studentForm", domString);
+}
+
+
+formOnDom();
